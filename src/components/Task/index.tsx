5cf10b23b9e769ee71737c3b70029746fc522c6b
Cardoso <cardosoassuncao@gmail.com>
2022-08-21 18:57:32 -0400

    finish app

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,4 +1,6 @@
 import { CheckCircle, Circle, Trash } from 'phosphor-react'
+import { useContext } from 'react'
+import { TasksContext } from '../../contexts/TasksContext'
 import { TaskContainer } from './styles'
 
 type TaskProps = {
@@ -10,19 +12,29 @@ type TaskProps = {
 }
 
 export function Task({ task }: TaskProps) {
+  const { deleteTask, updateTask } = useContext(TasksContext)
+
+  async function handleDeleteTask() {
+    await deleteTask(task.id)
+  }
+
+  async function handleUpdateTask() {
+    await updateTask(task)
+  }
+
   return (
     <TaskContainer isDone={task.isDone}>
       {task.isDone ? (
-        <button>
+        <button onClick={handleUpdateTask}>
           <CheckCircle size={24} />
         </button>
       ) : (
-        <button>
+        <button onClick={handleUpdateTask}>
           <Circle size={24} />
         </button>
       )}
       <span>{task.description}</span>
-      <button>
+      <button onClick={handleDeleteTask}>
         <Trash size={24} />
       </button>
     </TaskContainer>
--- src/contexts/TasksContext.tsx
+++ src/contexts/TasksContext.tsx
@@ -16,6 +16,8 @@ interface TaskContextType {
   tasks: Task[]
   fetchTasks: (query?: string) => Promise<void>
   createTask: (data: CreateTaskInput) => Promise<void>
+  deleteTask: (id: string) => Promise<void>
+  updateTask: (id: Task) => Promise<void>
 }
 
 interface TasksProviderProps {
@@ -30,12 +32,44 @@ export function TasksProvider({ children }: TasksProviderProps) {
   async function createTask(data: CreateTaskInput) {
     const { description } = data
     const newTask = { id: uuidv4(), description, isDone: false }
+
+    // vai lá no back-end e cria um novo registro
     const response = await api.post('/tasks', newTask)
+
+    // essa linha serve apenas para atualizar a tela
     setTasks((state) => [response.data, ...state])
   }
 
+  async function deleteTask(id: string) {
+    // vai lá no back-end e deleta o registro!
+    await api.delete(`/tasks/${id}`)
+
+    // essas duas linhas servem apenas para atualizar a tela!
+    const newTasks = tasks.filter((task) => task.id !== id)
+    setTasks([...newTasks])
+  }
+
+  async function updateTask(task: Task) {
+    const id = task.id
+    // vai lá do back-end e atualiza o registro!
+    await api.patch(`/tasks/${id}`, { ...task, isDone: !task.isDone })
+
+    // essas linhas servem apenas para atualizar a tela
+    const newTasks = tasks.map((task) => {
+      if (task.id === id) {
+        return { ...task, isDone: !task.isDone }
+      } else {
+        return task
+      }
+    })
+    setTasks([...newTasks])
+  }
+
   async function fetchTasks(query?: string) {
-    const response = await api.get('tasks', { params: { q: query } })
+    // vai lá no back-end e carrega as tasks
+    const response = await api.get('/tasks', { params: { q: query } })
+
+    // essa linha serve apenas para atualizar a tela
     setTasks(response.data)
   }
 
@@ -44,7 +78,9 @@ export function TasksProvider({ children }: TasksProviderProps) {
   }, [])
 
   return (
-    <TasksContext.Provider value={{ tasks, fetchTasks, createTask }}>
+    <TasksContext.Provider
+      value={{ tasks, fetchTasks, createTask, deleteTask, updateTask }}
+    >
       {children}
     </TasksContext.Provider>
   )
